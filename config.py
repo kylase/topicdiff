528bbd4711afc856c18f28c665ffb955f5d1e3a8
Kee Yuan Chuan <yuanchuan@u.nus.edu>
2018-05-26 14:28:17 +0800

    Get secret from environment variable

--- config.py
+++ config.py
@@ -6,5 +6,5 @@ BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 THREADS_PER_PAGE = 2
 
 CSRF_ENABLED = True
-CSRF_SESSION_KEY = "secret"
-SECRET_KEY = "secret"
+CSRF_SESSION_KEY = os.getenv('APP_SECRET')
+SECRET_KEY = os.getenv('APP_SECRET')
