ee10e2f48ffb6bedf9578f93dd038d575d78b259
Kee Yuan Chuan <yuanchuan@u.nus.edu>
2018-05-26 17:27:49 +0800

    Fixed deploy port

--- run.py
+++ run.py
@@ -7,5 +7,5 @@ CONFIG = DevelopmentConfig if get_debug_flag() else ProductionConfig
 
 app = create_app(CONFIG)
 
-app.run(host=os.getenv('APP_HOST', default='0.0.0.0'), 
-        port=os.getenv('APP_PORT', default=5000))
+app.run(host=os.getenv('HOST', default='0.0.0.0'), 
+        port=os.getenv('PORT', default='5000'))
