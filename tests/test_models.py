1d4792823f6b2df0b616e7f861f0aecd0a763ac8
Kee Yuan Chuan <yuanchuan@u.nus.edu>
2018-05-27 17:37:05 +0800

    Test the infer results for tuples

--- tests/test_models.py
+++ tests/test_models.py
@@ -29,4 +29,6 @@ def test_pipeline_infer(app):
 
     pipeline = TopicModelPipeline(model_files['tf'], model_files['lda'])
     text = """USS Orizaba was a transport ship for the U.S. Navy in World War I and World War II, first commissioned on 27 May 1918. Orizaba made 15 transatlantic voyages for the Navy carrying troops to and from Europe in World War I with the second-shortest average in-port turnaround time of all Navy transports. The ship was turned over to the War Department in 1919 for use as Army transport USAT Orizaba. After the war, the troopship reverted to the Ward Line, her previous owners. In World War II the ship was requisitioned by the War Shipping Administration and again assigned to the War Department, but was soon transferred to the U.S. Navy as USS Orizaba (AP-24). The ship made several transatlantic runs, was damaged in an air attack in the Allied invasion of Sicily, made trips to South America, and served in the Pacific Theatre. In June 1945 the ship was transferred under Lend-Lease to the Brazilian Navy, where she served as Duque de Caxias (U-11). Permanently transferred to Brazil in 1953, the ship was decommissioned in 1959 and scrapped in 1963."""
-    assert isinstance(pipeline.infer(text), list)
+    topics = pipeline.infer(text)
+    for topic in topics:
+        assert isinstance(topic, (tuple))
