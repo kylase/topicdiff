9d418970855336dda351c97fb1da69c32a7d628e
Kee Yuan Chuan <yuanchuan@u.nus.edu>
2018-05-31 00:05:33 +0800

    #6 Added Swagger and make navigation bar collapsible

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,36 @@
 from flask import Flask, Blueprint, render_template
-from flask_restful import Api
+from flask_restful_swagger_2 import Api, get_swagger_blueprint
+from flask_swagger_ui import get_swaggerui_blueprint
 from app.resources.documents import Compare
 
 def create_app(config):
     app = Flask(__name__)
     app.config.from_object(config)
 
+    API_DOC_PATH = '/api/docs'
+    SWAGGER_PATH = '/api/swagger'
+
     api_bp = Blueprint('api', __name__)
-    api = Api(api_bp)
+    api = Api(api_bp, add_api_spec_resource=False)
     api.add_resource(Compare, '/api/documents/compare')
-    app.register_blueprint(api_bp)
+
+    docs = []
+    docs.append(api.get_swagger_doc())
+
+    swagger_ui_blueprint = get_swaggerui_blueprint(
+        API_DOC_PATH,
+        SWAGGER_PATH + '.json',
+        config={
+            'app_name': 'TopicDiff API'
+        }
+    )
+
+    app.register_blueprint(api.blueprint)
+    app.register_blueprint(get_swagger_blueprint(docs, SWAGGER_PATH, 
+                                                 title='TopicDiff API', 
+                                                 api_version='1.0',
+                                                 base_path='/'))
+    app.register_blueprint(swagger_ui_blueprint, url_prefix=API_DOC_PATH)
 
     @app.errorhandler(404)
     def not_found(error):
--- app/resources/documents.py
+++ app/resources/documents.py
@@ -1,21 +1,62 @@
 import os
 from collections import defaultdict
-from flask_restful import Resource, reqparse
+from flask_restful import reqparse
+from flask_restful_swagger_2 import swagger, Resource
 from flask import current_app
+from app.resources.schemas import CompareRequestBody
 from app.common.topic_model import TopicModelPipeline
 
-parser = reqparse.RequestParser()
-
-parser.add_argument('content', action='append', required=True)
-parser.add_argument('model', type=str, choices=('wikipedia',), trim=True, default='wikipedia')
-parser.add_argument('threshold', type=float, default=0.01)
-
 class Compare(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('content', action='append', required=True)
+    parser.add_argument('model', type=str, choices=('wikipedia',), trim=True, default='wikipedia')
+    parser.add_argument('threshold', type=float, default=0.01)
+
+    @swagger.doc({
+        'tags': ['Documents'],
+        'description': 'Infer the topics which the document(s) is/are associated to.',
+        'parameters': [
+            {
+                'description': 'Content that is/are going to be parsed and inferred by the topic model with certain threshold.',
+                'in': 'body',
+                'name': 'body',
+                'required': True,
+                'schema': CompareRequestBody
+            }
+        ],
+        'responses': {
+            '200': {
+                'description': 'Successfully parsed the document(s) and inferred the topics.',
+                'examples': {
+                    'application/json': {
+                        "data": {
+                            "1": {
+                                "0": 0.17226679623126984,
+                                "1": 0.08550900220870972
+                            },
+                            "10": {
+                                "0": 0.08550900220870972
+                            },
+                            "77": {
+                                "0": 0.07097268104553223
+                            }
+                        },
+                        "model": {
+                            "name": "wikipedia",
+                            "total_topics": 100,
+                            "threshold": 0.05
+                        }
+                    }
+                }
+            },
+            '400': {
+                'description': 'Body is not valid, such as no content is included in the body or model is not valid or threshold is out of range.'
+            }
+        }
+    })
+
     def post(self):
-        """
-        Submit 1 or more `content`
-        """
-        args = parser.parse_args()
+        args = self.parser.parse_args()
         model_type = args.get('model')
         threshold = args.get('threshold')
 
