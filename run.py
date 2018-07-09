270da611097e5987689efbf43cd0a3a96ae59de7
Yuan Chuan Kee <kylase@outlook.com>
2018-07-09 20:59:01 +0800

    Add Sentry

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import os
 from flask.helpers import get_debug_flag
+from raven.contrib.flask import Sentry
 from app import create_app
 from app.settings import ProductionConfig, DevelopmentConfig
 
@@ -7,6 +8,8 @@ CONFIG = DevelopmentConfig if get_debug_flag() else ProductionConfig
 
 app = create_app(CONFIG)
 
+sentry = Sentry(app)
+
 if __name__ == '__main__':
-    app.run(host=os.getenv('HOST', default='0.0.0.0'), 
+    app.run(host=os.getenv('HOST', default='0.0.0.0'),
             port=int(os.getenv('PORT', default='5000')))
